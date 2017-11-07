21e0e9c465f7a87d051fdcc0332c509ccccc8300
Prasid <prasidmitra@gmail.com>
2017-11-06 22:42:04 -0700

    Update
    Make changes to the findIncident module

--- c.py
+++ c.py
@@ -5,11 +5,10 @@ synonyms = []
 antonyms = []
 wnl = WordNetLemmatizer()
 
-for syn in wordnet.synsets("kidnaps"):
+for syn in wordnet.synsets("arson"):
     for l in syn.lemmas():
         synonyms.append(l.name())
         if l.antonyms():
             antonyms.append(l.antonyms()[0].name())
 
-for i in synonyms:
-	print(wnl.lemmatize(i),i)
+print(list(set(synonyms)))
--- infoextract.py
+++ infoextract.py
@@ -7,7 +7,9 @@ from nltk.stem import PorterStemmer
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.tag import StanfordNERTagger
 from nltk.tokenize import word_tokenize
+from pathlib import Path
 
+#Change the path to the stanford ner
 st = StanfordNERTagger('/Users/prasidmitra/Downloads/stanford/stanford-ner-2017-06-09/classifiers/english.all.3class.distsim.crf.ser.gz','/Users/prasidmitra/Downloads/stanford/stanford-ner-2017-06-09/stanford-ner.jar',encoding='utf-8')
 
 ps = PorterStemmer()
@@ -18,15 +20,32 @@ ps = PorterStemmer()
  
 
 
-stories=[]
-story_objects=[]
+
 
 def findIncident(story,obj):
     incidents = ['arson','attack','bombing','kidnapping','robbery']
     incidents_synonyms = []
     for i in range(len(incidents)):
         incidents_synonyms.append(findSynonym(incidents[i]))
-    story_words = word_tokenize(story)
+    story_words = [ps.stem(i.lower()) for i in word_tokenize(story)]
+    score = [0,0,0,0,0]
+    for i in range(5):
+        for j in incidents_synonyms[i]:
+            score[i]+=story_words.count(j)
+            if i == 2:
+                print(j,story_words.count(j))
+    max = 0
+    bestindex = 0   
+    for i in range(5):
+        if i!=1:
+            if score[i]>max:
+                bestindex = i
+                max = score[i]
+    if max > 0:
+        obj.incident = incidents[bestindex]
+    else:
+        obj.incident = incidents[1]
+          
 
 def findSynonym(word):
     synonyms = []
@@ -37,9 +56,11 @@ def findSynonym(word):
             synonyms.append(l.name())
             if l.antonyms():
                 antonyms.append(l.antonyms()[0].name())
-    return synonyms            
+    return list(set(synonyms))            
 
 def extractInfo(textfile):
+    stories=[]
+    story_objects=[]
     f = open(textfile,'r')
     text = ""
     for line in f:
@@ -57,7 +78,8 @@ def extractInfo(textfile):
         stories.append(text)
 
     for i in range(len(stories)):
-        findIncident(stories[i],story_objects[i])
+        findIncident(stories[i],story_objects[i])   
+    return story_objects[0]   
 
 
     #   Stanford NER Tagger
@@ -75,15 +97,47 @@ def extractInfo(textfile):
     #   print(i.text, i.ent_iob,i.ent_type_)   
 
         
+def checkAccuracy():
+    
+    count  = 0
+    filecount = 0
+    acc = [0,0,0,0,0,0,0]
+    while(count<=1300):
+        testfile='./developset/texts/DEV-MUC3-'+str(count).zfill(4)
+        ansfile='./developset/answers/DEV-MUC3-'+str(count).zfill(4)+'.anskey'
+        if Path(testfile).is_file():
+            obj = extractInfo(testfile)
+
+            #incident
+            incident_actual = ''
+
+            f = open(ansfile)
+            for line in f:
+                nline = line.strip().split()
+                if nline[0]=='INCIDENT:':
+                    incident_actual = nline[1].lower()
+                    break
+            f.close()
+
+            incident_pred = obj.incident
+            if incident_pred == incident_actual:
+                acc[1]+=1
+            print (obj.id,incident_pred, incident_actual)
+            filecount+=1        
+
+        count+=1
+
+    print('incident accuracy = ', acc[1]/filecount)    
 
 
 
 def main():
     #args=sys.argv
     #textfile = './developset/texts/DEV-MUC3-0006'
-    textfile = './developset/texts/sample.txt'
+    #textfile = './developset/texts/sample1.txt'
+    checkAccuracy()
 
-    extractInfo(textfile)
+    #extractInfo(textfile)
 
 if __name__ == '__main__':
     main()

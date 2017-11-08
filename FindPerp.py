8b7869b39432b706f4835a3336d87b0c16268564
Prasid <prasidmitra@gmail.com>
2017-11-07 21:45:54 -0700

    Update
    Find Perp

---
+++ FindPerp.py
@@ -0,0 +1,62 @@
+from nltk.parse.stanford import StanfordDependencyParser
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+from nltk.tag import StanfordNERTagger
+from nltk.stem import PorterStemmer
+from FindPerp import FindPerp
+
+import spacy
+nlp = spacy.load('en')
+ps = PorterStemmer()
+
+seedwords = ['attack','attacked','bomb','bombed','grenade','destroyed','destroy']
+
+#dep_parser=StanfordDependencyParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
+#st = StanfordNERTagger('/Users/prasidmitra/Downloads/stanford/stanford-ner-2017-06-09/classifiers/english.all.3class.distsim.crf.ser.gz','/Users/prasidmitra/Downloads/stanford/stanford-ner-2017-06-09/stanford-ner.jar',encoding='utf-8')
+def FindPerp(story,storyobj):
+    sentences = sent_tokenize(story)
+    index =0
+    for s in sentences:
+        # s.replace('\n',' ').replace('\r','')
+        # doc = nlp(s)
+        # print('*********')
+        # noun_phrases=[np.text for np in doc.noun_chunks]
+        # depend = [token.text+' '+token.dep_ for token in doc]
+        # print(noun_phrases)
+        # print('*********')
+        # print(depend)
+        # print('*********')
+        # print(st.tag(word_tokenize(s)))
+        # print('____________________________________________')
+        # result = [list(i.triples()) for i in dep_parser.raw_parse(s)]
+        # print(result)
+        flag =0
+        count=0
+        for word in seedwords:
+            sentence_words = [ps.stem(i.lower()) for i in word_tokenize(s)]
+            if word in sentence_words:
+                count=1
+                flag = 1
+                break
+        if flag==1:
+            break
+        index +=1    
+    if count == 0:
+        storyobj.indiv='-'
+    else:
+        s = sentences[index]
+        doc=nlp(s)
+        text = ''
+        for token in doc:
+            if token.dep_ == 'nsubj':
+                text=token.text
+                break
+        noun_phrases=[np.text for np in doc.noun_chunks]
+        if len(text)>0:    
+            for i in noun_phrases:
+                np_split = i.split()
+                if text in np_split:
+                    storyobj.indiv=i.upper()
+                    break
+        else:
+            storyobj.indiv=noun_phrases[-1].upper() 
